3628875330cfcd731d3b2ca856032d4d12003c01
Website-Tourism <gourav9876chout@gmail.com>
2023-03-19 16:15:54 +0530

    Save changes

--- index.js
+++ index.js
@@ -7,19 +7,19 @@
 
     window.addEventListener('scroll',reveal);
 
-    function reveal(){
-        var reveals =document.querySelectorAll('.reveal');
+//     function reveal(){
+//         var reveals =document.querySelectorAll('.reveal');
 
-        for(var i=0; i< reveals.length; i++){
-            var windowHeight = window.innerHeight;
-            var revealtop= reveals[i].getBoundingClientRect().top;
-            var revealpoint = 150;
+//         for(var i=0; i< reveals.length; i++){
+//             var windowHeight = window.innerHeight;
+//             var revealtop= reveals[i].getBoundingClientRect().top;
+//             var revealpoint = 150;
             
-            if(revealtop < windowHeight - revealpoint){
-                reveals[i].classList.add('active');
-                }
-        else{
-            reveals[i].classList.remove('active');k
-        }
-    }
-}
+//             if(revealtop < windowHeight - revealpoint){
+//                 reveals[i].classList.add('active');
+//                 }
+//         else{
+//             reveals[i].classList.remove('active');k
+//         }
+//     }
+// }
